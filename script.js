0d49e82bd7f5137cc7e5e0f08d76e9f82252430e
raditnare90-lgtm <raditnare90@gmail.com>
2026-03-25 06:34:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// --- BAGIAN EDIT DI SINI ---
+const NOMOR_BANK = "randuwe bank";
+const URL_GAMBAR_QRIS = "https://ibb.co.com/QvVfvxP0"; 
+// ---------------------------
+
+// Set nomor bank otomatis di menu
+document.getElementById('bankName').innerText = NOMOR_BANK;
+
+function showQRIS() {
+    let nominal = document.getElementById('inputNominal').value;
+    
+    if(nominal === "" || nominal <= 0) {
+        alert("Harap masukkan nominal dana dulu!");
+        return;
+    }
+
+    // Pindah Tampilan
+    document.getElementById('main-view').classList.add('hidden');
+    document.getElementById('qris-view').classList.remove('hidden');
+    
+    // Set nominal di layar QRIS
+    document.getElementById('displayTotal').innerText = "Rp " + parseInt(nominal).toLocaleString();
+
+    // Ganti Gambar QRIS (Jika link kosong, dia pakai QR Generator otomatis)
+    let qrisImg = document.getElementById('qrisImage');
+    if(URL_GAMBAR_QRIS.includes("http")) {
+        qrisImg.src = URL_GAMBAR_QRIS;
+    } else {
+        qrisImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=Bayar_Rp_${nominal}`;
+    }
+}
+
+function goBack() {
+    document.getElementById('qris-view').classList.add('hidden');
+    document.getElementById('main-view').classList.remove('hidden');
+}
+
+function showBank() {
+    let nominal = document.getElementById('inputNominal').value;
+    if(nominal <= 0) {
+        alert("Masukkan nominal dulu!");
+    } else {
+        alert("Silakan transfer Rp " + parseInt(nominal).toLocaleString() + "\nKe: " + NOMOR_BANK);
+    }
+}
+
+function finish() {
+    alert("Terima kasih! Pesanan Anda sedang dicek.");
+    location.reload();
+}
